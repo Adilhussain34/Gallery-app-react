67b2d24c5d2592ff4d88da968b16fe1ad7b9087d
Adil hussain <101266575+Adilhussain34@users.noreply.github.com>
2023-02-28 14:41:43 +0530

    Gallery-app

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+// optional configuration
+const options = {
+    // you can also just use 'bottom center'
+    position: positions.BOTTOM_CENTER,
+    timeout: 1500,
+    offset: "30px",
+    // you can also just use 'scale'
+    transition: transitions.SCALE,
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+    </AlertProvider>
+);
